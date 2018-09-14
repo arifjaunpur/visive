c5b2954d3c1769847ce8af9f372b6d599e423f92
Mohd Arif Khan <mohdarifkhan@Mohds-MacBook-Air.local>
2018-09-14 12:55:54 +0530

    added simple lambda function

---
+++ lambda/index.js
@@ -0,0 +1,8 @@
+exports.handler = async (event, context) => {
+  const name = event.queryStringParameters.name || "World";
+  console.log('event.queryStringParameters ', event.queryStringParameters)
+  return {
+    statusCode: 200,
+    body: `Hello, ${name}`
+  };
+};
